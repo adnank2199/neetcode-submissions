0100b95c4c68547300047c2024ad31570ac90250
Mohammad Adnan Khan <adnankhanp107@gmail.com>
2026-09-24 21:21:20 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==0)
+        return 0;
+        if(nums.length==1)
+        return nums[0];
+
+        int a = nums[0];
+        int b = Math.max(nums[0],nums[1]);
+
+        for(int i = 2 ; i < nums.length ; i++){
+            int temp = b ; 
+            b = Math.max(b,a+nums[i]);
+            a=temp;
+        }
+
+        return b;
+    }
+}
